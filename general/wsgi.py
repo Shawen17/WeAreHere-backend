39e7efc0eff77b6422fd263c422248b775796777
shawen17 <shawen022@yahoo.com>
2023-09-23 08:06:46 +0100

    vercel variables

--- general/wsgi.py
+++ general/wsgi.py
@@ -14,3 +14,11 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'general.settings')
 
 application = get_wsgi_application()
+
+app = application
+
+# import os
+# from django.core.wsgi import get_wsgi_application
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
+# application = get_wsgi_application()
+# app = application
