eef074eb46dff70a4066dcd9fef733f2348f8bbc
shawen17 <shawen022@yahoo.com>
2023-09-23 08:16:42 +0100

    wsgi changed

--- general/wsgi.py
+++ general/wsgi.py
@@ -8,13 +8,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'general.settings')
-
 application = get_wsgi_application()
-
 app = application
 
 # import os
