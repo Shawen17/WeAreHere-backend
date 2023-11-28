0b735e5570f677ed703f6d6edb58d6719ce06c5f
shawen17 <shawen022@yahoo.com>
2023-11-28 11:15:12 +0100

    CORS updated

--- general/settings.py
+++ general/settings.py
@@ -186,7 +186,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'https://fascinating-marzipan-f7a73a.netlify.app',
-    'https://wearehere.ng']
+    'https://www.wearehere.ng']
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
