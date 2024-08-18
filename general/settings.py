19bbd00eeec4ba2b844a3c4342e945e834462345
shawen17 <shawen022@yahoo.com>
2024-08-17 21:04:26 -0400

    database change

--- general/settings.py
+++ general/settings.py
@@ -1,12 +1,9 @@
-
-
 from pathlib import Path
 import os
 from datetime import timedelta
 from dotenv import load_dotenv
 
 load_dotenv()
-
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,57 +15,57 @@ SECRET_KEY = os.getenv("SECRET_KEY", "some value if your key is not in the envir
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['we-are-here-backend.vercel.app','localhost','0.0.0.0']
+ALLOWED_HOSTS = ["we-are-here-backend.vercel.app", "localhost", "0.0.0.0"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'here',
-    'rest_framework',
-    'djoser',
-    'corsheaders',
-    'whitenoise.runserver_nostatic',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "here",
+    "rest_framework",
+    "djoser",
+    "corsheaders",
+    "whitenoise.runserver_nostatic",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+    "django.middleware.security.SecurityMiddleware",
     "corsheaders.middleware.CorsMiddleware",
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'general.urls'
+ROOT_URLCONF = "general.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.template.context_processors.media',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.template.context_processors.media",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'general.wsgi.application'
+WSGI_APPLICATION = "general.wsgi.application"
 
 
 # Database
@@ -82,17 +79,16 @@ WSGI_APPLICATION = 'general.wsgi.application'
 # }
 
 DATABASES = {
-    'default': {
-    'ENGINE': 'django.db.backends.postgresql',
-    'URL': os.environ.get('URL'),
-    'NAME': 'railway',
-    'USER': 'postgres',
-    'PASSWORD': os.getenv('PASSWORD'),
-    'HOST': os.getenv('HOST'),
-    'PORT': 10036,
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "URL": os.environ.get("URL"),
+        "NAME": "railway",
+        "USER": "postgres",
+        "PASSWORD": os.getenv("PASSWORD"),
+        "HOST": os.getenv("HOST"),
+        "PORT": 51626,
+    }
 }
-}
-
 
 
 # Password validation
@@ -100,116 +96,112 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-         'rest_framework.permissions.AllowAny',
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
         # 'rest_framework.permissions.IsAuthenticatedOrReadOnly',
         # 'rest_framework.permissions.IsAuthenticated',
-        
     ],
-    'DEFAULT_AUTHENTICATION_CLASSES':(
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'django.contrib.auth.backends.ModelBackend',
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "django.contrib.auth.backends.ModelBackend",
         # 'allauth.account.auth_backends.AuthenticationBackend',
-        'rest_framework.authentication.TokenAuthentication',
-        
-        ),
+        "rest_framework.authentication.TokenAuthentication",
+    ),
 }
 
 SIMPLE_JWT = {
-   'AUTH_HEADER_TYPES': ('JWT',),
-   'ACCESS_TOKEN_LIFETIME':timedelta(minutes=120),
-   'REFRESH_TOKEN_LIFETIME':timedelta(days=1),
+    "AUTH_HEADER_TYPES": ("JWT",),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=120),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
 
-AUTH_USER_MODEL='here.User'
+AUTH_USER_MODEL = "here.User"
 
 DJOSER = {
     # 'EMAIL': {
     #         'activation': 'here.email.ActivationEmail'
     # },
-    'LOGIN_FIELD':'email',
-    'USER_CREATE_PASSWORD_RETYPE':True,
-    'PASSWORD_CHANGED_EMAIL_CONFIRMATION':True,
-    'SEND_CONFIRMATION_EMAIL':False,
-    'SET_PASSWORD_RETYPE':True,
-    'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
+    "LOGIN_FIELD": "email",
+    "USER_CREATE_PASSWORD_RETYPE": True,
+    "PASSWORD_CHANGED_EMAIL_CONFIRMATION": True,
+    "SEND_CONFIRMATION_EMAIL": False,
+    "SET_PASSWORD_RETYPE": True,
+    "PASSWORD_RESET_CONFIRM_URL": "password/reset/confirm/{uid}/{token}",
     # 'ACTIVATION_URL': 'activate/{uid}/{token}',
-    'SEND_ACTIVATION_EMAIL': False,
-    'SERIALIZERS': {
-        'user_create':'here.serializers.UserCreateSerializer',
-        'user':'here.serializers.UserSerializer',
-        'user_delete':'djoser.serializers.UserDeleteSerializer',
+    "SEND_ACTIVATION_EMAIL": False,
+    "SERIALIZERS": {
+        "user_create": "here.serializers.UserCreateSerializer",
+        "user": "here.serializers.UserSerializer",
+        "user_delete": "djoser.serializers.UserDeleteSerializer",
     },
 }
-
 
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
 USE_TZ = True
 
 
-
-
-# STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
 # STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'shawenmedia'
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_OBJECT_PARAMETERS = {"CacheControl" : "max-age=86400"}
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = "shawenmedia"
+AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
 # AWS_DEFAULT_ACL = 'public-read-write'
-AWS_LOCATION = 'staticfiles'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'staticfiles')]
-STATICFILES_STORAGE='storages.backends.s3boto3.S3Boto3Storage'
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-DEFAULT_FILE_STORAGE = 'general.storages.MediaStore'
+AWS_LOCATION = "staticfiles"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
+# STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+# STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/"
+# DEFAULT_FILE_STORAGE = "general.storages.MediaStore"
 
 # STATIC_URL = 'static/'
-MEDIA_DIR = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
-MEDIA_URL = '/media/'
+MEDIA_DIR = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
+MEDIA_URL = "/media/"
 
-# STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 CORS_ALLOWED_ORIGINS = [
-    'https://we-are-here-backend.vercel.app',
-    'http://localhost:3000',
-    'https://fascinating-marzipan-f7a73a.netlify.app',
-    'https://www.wearehere.ng']
+    "https://we-are-here-backend.vercel.app",
+    "http://localhost:3000",
+    "https://fascinating-marzipan-f7a73a.netlify.app",
+    "https://www.wearehere.ng",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-EMAIL_USE_SSL= True
+EMAIL_USE_SSL = True
 EMAIL_USE_TLS = False
 DEFAULT_FROM_EMAIL = os.getenv("EMAIL_HOST_USER")
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
@@ -217,6 +209,3 @@ EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = os.getenv("EMAIL_PORT")
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-
-
-
--- general/urls.py
+++ general/urls.py
@@ -2,37 +2,46 @@ from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
 from here.views import UserViewSet
 from django.contrib import admin
-from django.conf.urls.static import static 
-from  django.conf import settings
+from django.conf.urls.static import static
+from django.conf import settings
 from here import views
 from django.views.static import serve
 
 
 router = DefaultRouter()
-router.register(r'users', UserViewSet)
+router.register(r"users", UserViewSet)
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
-    path('api/book/', views.make_appointment, name='make_appointment' ),
-    path('api/appointments/', views.fetch_appointment, name='fetch_appointment' ),
-    path('api/add-property/', views.add_property, name='add_property' ),
-    path('api/get-property/', views.get_property_admin, name='get_property_admin' ),
-    path('api/properties/', views.get_properties, name='get_properties' ),
-    path('api/realestate-booking/', views.realestate_booking, name='realestate_booking' ),
-    path('api/delete-property/<int:id>/', views.delete_property, name='delete_property' ),
-    path('api/delete-properties/', views.delete_properties, name='delete_properties' ),
-    path('api/update-bookings/', views.update_bookings, name='update_bookings' ),
-    path('api/charges/', views.get_charges, name='get_charges' ),
-    path('api/contact/', views.contact_us, name='contact_us' ),
-    path('api/initiate-payment/', views.initiate_subscription, name='initiate_subscription' ),
-
+    path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
+    path("auth/", include("djoser.urls")),
+    path("auth/", include("djoser.urls.jwt")),
+    path("api/book/", views.make_appointment, name="make_appointment"),
+    path("api/appointments/", views.fetch_appointment, name="fetch_appointment"),
+    path("api/add-property/", views.add_property, name="add_property"),
+    path("api/get-property/", views.get_property_admin, name="get_property_admin"),
+    path("api/properties/", views.get_properties, name="get_properties"),
+    path(
+        "api/realestate-booking/", views.realestate_booking, name="realestate_booking"
+    ),
+    path(
+        "api/delete-property/<int:id>/", views.delete_property, name="delete_property"
+    ),
+    path("api/delete-properties/", views.delete_properties, name="delete_properties"),
+    path("api/update-bookings/", views.update_bookings, name="update_bookings"),
+    path("api/charges/", views.get_charges, name="get_charges"),
+    path("api/contact/", views.contact_us, name="contact_us"),
+    path(
+        "api/initiate-payment/",
+        views.initiate_subscription,
+        name="initiate_subscription",
+    ),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,document_root= settings.MEDIA_ROOT)
-urlpatterns += [re_path(r'^media/(?P<path>.*)', serve, {'document_root':settings.MEDIA_DIR})]   
-urlpatterns += static(settings.AWS_LOCATION,document_root= settings.STATIC_URL)  
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r"^media/(?P<path>.*)", serve, {"document_root": settings.MEDIA_DIR})
+]
+urlpatterns += static(settings.AWS_LOCATION, document_root=settings.STATIC_URL)
